c3e62a43338248e918ff4fc5daa4657c7b63373a
小傅Fox <coderfox.fu@gmail.com>
2015-05-02 15:58:10 +0800

    separate /reg functional code

--- routes/main/index.js
+++ routes/main/index.js
@@ -19,64 +19,6 @@ router.get('/reg', function(req, res, next) {
   });
 });
 // POST /reg
-router.post('/reg', function(req, res, next) {
-  var regFunction = require('../../functions/reg');
-  // initialize data object
-  var data = {};
-  data.user = req.body.username;
-  data.pass = req.body.password;
-  data.pass2 = req.body['password-check'];
-  // check params
-  if (!data.user) {
-    res.render('main/reg', {
-      title: config.get('site.front'),
-      banner: config.get('content.front.banner'),
-      home_title: config.get('site.front'),
-      error: 'Parameter \'username\' cannot be null.'
-    });
-    return;
-  }
-  if (!data.pass) {
-    res.render('main/reg', {
-      title: config.get('site.front'),
-      banner: config.get('content.front.banner'),
-      home_title: config.get('site.front'),
-      error: 'Parameter \'password\' cannot be null.'
-    });
-    return;
-  }
-  if (data.pass !== data.pass2) {
-    res.render('main/reg', {
-      title: config.get('site.front'),
-      banner: config.get('content.front.banner'),
-      home_title: config.get('site.front'),
-      error: 'Parameter \'password\' mismatches \'password-check\'.'
-    });
-    return;
-  }
-  // call to reg
-  regFunction(data.user,
-    data.pass,
-    function(err) {
-      if (err) {
-        r.l.debug('reg error')
-        res.render('main/reg', {
-          title: config.get('site.front'),
-          banner: config.get('content.front.banner'),
-          home_title: config.get('site.front'),
-          error: err.message
-        });
-        return;
-      } else {
-        r.l.debug('reg ok')
-        res.render('main/reg', {
-          title: config.get('site.front'),
-          banner: config.get('content.front.banner'),
-          home_title: config.get('site.front'),
-          success: true
-        });
-      }
-    });
-});
+router.post('/reg', require('./reg.post'));
 
 module.exports = router;
---
+++ routes/main/reg.post.js
@@ -0,0 +1,59 @@
+module.exports = function(req,res,next){
+  var regFunction = require('../../functions/reg');
+  // initialize data object
+  var data = {};
+  data.user = req.body.username;
+  data.pass = req.body.password;
+  data.pass2 = req.body['password-check'];
+  // check params
+  if (!data.user) {
+    res.render('main/reg', {
+      title: r.c('site.front'),
+      banner: r.c('content.front.banner'),
+      home_title: r.c('site.front'),
+      error: 'Parameter \'username\' cannot be null.'
+    });
+    return;
+  }
+  if (!data.pass) {
+    res.render('main/reg', {
+      title: r.c('site.front'),
+      banner: r.c('content.front.banner'),
+      home_title: r.c('site.front'),
+      error: 'Parameter \'password\' cannot be null.'
+    });
+    return;
+  }
+  if (data.pass !== data.pass2) {
+    res.render('main/reg', {
+      title: r.c('site.front'),
+      banner: r.c('content.front.banner'),
+      home_title: r.c('site.front'),
+      error: 'Parameter \'password\' mismatches \'password-check\'.'
+    });
+    return;
+  }
+  // call to reg
+  regFunction(data.user,
+    data.pass,
+    function(err) {
+      if (err) {
+        r.l.debug('reg error')
+        res.render('main/reg', {
+          title: r.c('site.front'),
+          banner: r.c('content.front.banner'),
+          home_title: r.c('site.front'),
+          error: err.message
+        });
+        return;
+      } else {
+        r.l.debug('reg ok')
+        res.render('main/reg', {
+          title: r.c('site.front'),
+          banner: r.c('content.front.banner'),
+          home_title: r.c('site.front'),
+          success: true
+        });
+      }
+    });
+}
