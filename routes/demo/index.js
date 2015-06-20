60ac9cea397f71cdcf485fc54c2bb1fcdd7196b8
小傅Fox <coderfox.fu@gmail.com>
2015-06-20 19:37:29 +0800

    new demo

--- routes/demo/index.js
+++ routes/demo/index.js
@@ -7,17 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('demo/index', {
     title: config.get('site.demo'),
     banner: config.get('content.demo.banner'),
-    home_title: config.get('site.demo')
+    home_title: config.get('site.demo'),
+    uri: 'rapid://authorize/?callback=' + encodeURIComponent(config.get('server.base') + 'demo/callback?token=') +
+      '&app=916FA0569B0135402A84FF2506C3CEC553241D568A0796F9F08A603AFB6BCB036FDC4A61DE218540CFAD0A7A7D52AC1345EDAFDCC02CC5330311DC40CDB6B3F3E36540ED759398F89F8A913E469805C497FB6DEE0E7B95A4'
   });
 });
 // GET /callback
-router.get('/callback', function(req,res,next){
-  res.render('demo/index',{
+router.get('/callback', function(req, res, next) {
+  res.render('demo/callback', {
     title: config.get('site.demo'),
     banner: config.get('content.demo.banner'),
     home_title: config.get('site.demo'),
-    token: req.query.token
-  })
+    token: req.query.token,
+    demo_callback: true,
+    api: config.get('server.api')
+  });
 });
 
 module.exports = router;
