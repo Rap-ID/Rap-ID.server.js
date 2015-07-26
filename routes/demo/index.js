e5b6f57f7cf8e0ff7b393333d55d1ec00b8c3f5e
小傅Fox <coderfox.fu@gmail.com>
2015-07-26 18:24:19 +0800

    Demo Adaptation to New Auth Page

--- routes/demo/index.js
+++ routes/demo/index.js
@@ -4,12 +4,13 @@ var config = require('config');
 
 // GET /
 router.get('/', function(req, res, next) {
+  var querystring=require('querystring');
   res.render('demo/index', {
     title: config.get('site.demo'),
     banner: config.get('content.demo.banner'),
     home_title: config.get('site.demo'),
-    uri: 'rapid://authorize/?callback=' + encodeURIComponent(config.get('server.base') + 'demo/callback?token=') +
-      '&app=88587CD267E00E016B545E38FD133FCA077535F9B91C0E246FCD48AA5C77996DC935833C27C9A0EC0888D15ACD941C0B461716F61EAC6B7410BB9C46642B0808BF4C32E261A40F385070B87F9CBCE3B51B20E83A8D11B08D'
+    uri: config.get('server.base')+'api/auth?'+querystring.stringify({callback: config.get('server.base') + 'demo/callback?token=', 
+      app: '88587CD267E00E016B545E38FD133FCA077535F9B91C0E246FCD48AA5C77996DC935833C27C9A0EC0888D15ACD941C0B461716F61EAC6B7410BB9C46642B0808BF4C32E261A40F385070B87F9CBCE3B51B20E83A8D11B08D'})
   });
 });
 // GET /callback
