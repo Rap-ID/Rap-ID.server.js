0820fdf7a55b39b475ce9ce5fa81592a4033ec32
小傅Fox <coderfox.fu@gmail.com>
2015-07-27 22:01:42 +0800

    Allow POST Request to /api/auth

--- routes/api/index.js
+++ routes/api/index.js
@@ -12,15 +12,24 @@ router.post('/login', require('./login'));
 router.post('/reg', require('./reg'));
 // GET /user/(:token)
 router.get('/user', require('./user'));
-// GET /auth
-router.get('/auth', function (req, res, next) {
+// GET /auth & POST /auth
+var auth = function (req, res, next) {
   var querystring = require('querystring');
+  var getParam = function (name) {
+    return req.query[name] || req.body[name];
+  };
   res.render('api/auth', {
     title: config.get('site.front'),
     banner: config.get('content.front.banner'),
     home_title: config.get('site.front'),
-    callUrl: 'rapid://authorize/?' + querystring.stringify({ app: req.query.app, callback: req.query.callback })
+    callUrl: 'rapid://authorize/?' + querystring.stringify(
+      {
+        app: getParam('app'),
+        callback: getParam('callback')
+      })
   });
-});
+};
+router.get('/auth', auth);
+router.post('/auth', auth);
 
 module.exports = router;
